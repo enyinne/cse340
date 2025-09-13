0d4281782bacd736a1a9f22b1173ee3a2e02aada
YourNewUsername <yournewemail@example.com>
2025-09-13 19:26:23 +0100

    initiam commit

--- server.js
+++ server.js
@@ -6,9 +6,21 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+// index route
+app.get("/", function(req, res){
+  res.render("index", {titlt: "Home"})
+})
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
